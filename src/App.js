fe8a33632d5e2a2f86117f42fd20299ae5d7adb4
root <pvidalsalvador@gmail.com>
2021-01-12 12:30:17 +0100

    React_app created

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Funcionando el despliegue.</p>
+        <p>Funcionando el despliegue a full</p>
         <a
           className="App-link"
           href="https://reactjs.org"
