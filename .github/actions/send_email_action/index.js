258d3dbb1e6461c56c87f894b504e3083e3f9184
root <pvidalsalvador@gmail.com>
2021-01-19 10:17:32 +0100

    First custom github Action

---
+++ .github/actions/send_email_action/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const email_to_send = core.getInput("email_to_send");
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+var mailOptions = {
+  from: process.env.EMAIL_USER,
+  to: email_to_send,
+  subject: "Correo desde la Action personalizada",
+  text:
+    "Este correo verifica la correcta ejecución de la Action que hemos creado",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    core.setOutput("response", error);
+  } else {
+    core.setOutput("response", info.response);
+  }
+});
