dafc31d23895148e326c950caee80065a862a84d
root <pvidalsalvador@gmail.com>
2021-01-19 11:06:25 +0100

    First custom github Action

--- .github/actions/send_email_action/index.js
+++ .github/actions/send_email_action/index.js
@@ -3,8 +3,11 @@ const nodemailer = require("nodemailer");
 const core = require("@actions/core");
 const email_to_send = core.getInput("email_to_send");
 
-var transporter = nodemailer.createTransport({
-  service: "gmail",
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
