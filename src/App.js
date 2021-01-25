6d2116fb69c9b4ac92f116688d3b475973b51be2
jovisal1 <pvidalsalvador@gmail.com>
2021-01-25 18:02:21 +0000

    App.js modification with random gif

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="1" className="App-logo" alt="logo" />
         <p>Funcionando el despliegue a full</p>
         <a
           className="App-link"
